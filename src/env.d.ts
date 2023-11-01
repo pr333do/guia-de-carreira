03f5700c58d9343e85a396facc4a83ad91e53a4d
Pedro Soares <pedrosoares@aluno.puc-rio.br>
2023-10-31 22:42:18 -0300

    feat(ga): adding google analytics

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_GA_METRIC_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
