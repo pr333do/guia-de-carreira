3bb10a8b32eb80e03736414bc5ea5d06c0643e00
Pedro Soares <pedrosoares@aluno.puc-rio.br>
2023-11-01 11:19:08 -0300

    feat(partytown): adding partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
